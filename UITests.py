1335a6d8b8288be745b7ade90bb3b319e2849669
Shriganesh <shriganesh1987@gmail.com>
2019-04-14 10:34:53 +0530

    Added a basic test

--- UITests.py
+++ UITests.py
@@ -1,1 +1,18 @@
-print("My first GitHub file")
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as ec
+from selenium.webdriver.common.by import By
+
+class TestClass:
+    def test_browser(self):
+        driver = webdriver.Chrome()
+        driver.maximize_window()
+        driver.get('http://www.google.com')
+        driver.close()
+
+    def test_google(self):
+        driver = webdriver.Chrome()
+        driver.maximize_window()
+        driver.get('http://www.google.com')
+        WebDriverWait(driver, 60).until(ec.element_to_be_clickable((By.XPATH, "//input[@class='gLFyf gsfi']")))
+        driver.close()
